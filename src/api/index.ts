eba3380c43b45024a8749f1b6fa88650e18b54f7
mikhael-remboski <mikhael.remboski@justo.mx>
2024-10-20 21:11:34 -0300

    chore: disable x-powered-by

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,6 +8,8 @@ const bootstrap = (): Application => {
   const app: express.Application = express();
   app.use(express.json({ limit: '1mb' }));
   app.use(express.urlencoded({ extended: true }));
+  app.disable('x-powered-by');
+
   app.use(requestContextMiddleware);
   app.use(openApiRouter());
   app.use(cartRouter(getCartController()));
