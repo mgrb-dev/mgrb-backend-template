a4a29805d8b9c3a664afef077eb1b6da6c3a29e7
mikhael-remboski <mikhael.remboski@justo.mx>
2024-10-20 21:00:56 -0300

    feat: health check and four zero four middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,16 @@
 import express, { Application } from 'express';
 import requestContextMiddleware from '#shared/request-context/request-context.middleware';
+import openApiRouter from '#api/openapi/openapi.router';
+import cartRouter from '#api/routers/cart.router';
+import { getCartController } from '#api/inject/controllers';
 
 const bootstrap = (): Application => {
   const app: express.Application = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(requestContextMiddleware);
-
+  app.use(openApiRouter());
+  app.use(cartRouter(getCartController()));
   return app;
 };
 
--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,22 +1,17 @@
-import { Application, Request, Response } from 'express';
+import { Application } from 'express';
 import bootstrap from '#api';
-import cartRouter from '#api/routers/cart.router';
-import openApiRouter from '#api/openapi/openapi.router';
-import { getCartController } from '#api/inject/controllers';
 import errorHandlerMiddleware from '#shared/error-handler/error-handler.middleware';
 import 'dotenv/config';
+import { healthCheck } from '#shared/health/health-check';
+import { fourZeroFourMiddleware } from '#shared/404/four-zero-four.middleware';
+
+const HEALTH_CHECK_PATH = '/health';
 
 const app: Application = bootstrap();
-app.use(openApiRouter());
-app.use(cartRouter(getCartController()));
 
-app.get('/health', (_req: Request, res: Response) => {
-  res.send('Server is running!');
-});
-
-app.use((_req: Request, res: Response) => {
-  res.status(404).json({ message: 'Not Found' });
-});
+app.use(fourZeroFourMiddleware);
 app.use(errorHandlerMiddleware);
+
+app.get(HEALTH_CHECK_PATH, healthCheck);
 
 export default app;
