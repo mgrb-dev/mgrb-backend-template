d54c62daf5935c6a3a18f724e2fb5e0891990c29
mikhael-remboski <mikhael.remboski@justo.mx>
2024-10-20 21:02:38 -0300

    feat: health check and four zero four middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ import { getCartController } from '#api/inject/controllers';
 
 const bootstrap = (): Application => {
   const app: express.Application = express();
-  app.use(express.json({ limit: '5mb' }));
+  app.use(express.json({ limit: '1mb' }));
   app.use(express.urlencoded({ extended: true }));
   app.use(requestContextMiddleware);
   app.use(openApiRouter());
